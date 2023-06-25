99cc9afb0c44ffc03443b49030b4ed3bacf4c303
codding-nepale <79444135+codding-nepale@users.noreply.github.com>
2023-06-25 13:54:50 +0200

    Update 00ActivateDHCP.py

--- localscripts/00ActivateDHCP.py
+++ localscripts/00ActivateDHCP.py
@@ -1,21 +1,21 @@
-import os,json,sys,wmi
+import os
+import json
+import sys
+import wmi
 from cloudbaseinit.osutils import factory as osutils_factory
-from cloudbaseinit.utils import network
 
-
-
-def load_json_variable(file_path,variable):
+def load_json_variable(file_path, variable):
     file = open(file_path)
     data = json.load(file)
     file.close()
     return data.get(variable)
 
 def find_drive(file_path):
-    for number in range(65,91):
+    for number in range(65, 91):
         drive_letter = chr(number)
-        if os.path.exists(drive_letter+file_path):
-            return drive_letter+file_path
-    print("\n Searched file could not be found on any drive with path:" + file_path)
+        if os.path.exists(drive_letter + file_path):
+            return drive_letter + file_path
+    print("\nSearched file could not be found on any drive with path: " + file_path)
     return False
 
 def get_name_by_mac(mac):
@@ -27,19 +27,20 @@ def activate_dhcp(name, family):
     osutils = osutils_factory.get_os_utils()
     osutils._fix_network_adapter_dhcp(name, True, family)
 
-
-# variables
+# Variables
 meta_data_path = find_drive(":\OPENSTACK\LATEST\META_DATA.json")
-# 2 for ipv4 and 6 for ipv6
+# 2 for IPv4 and 6 for IPv6
 family = 2
-# execute
-if meta_data_path != "False":
-    macs = load_json_variable(meta_data_path,"dhcp")
-    
-    for mac in macs:
-        name = get_name_by_mac(mac)
-        activate_dhcp(name, family)
+
+# Execute
+if meta_data_path != False:
+    macs = load_json_variable(meta_data_path, "dhcp")
+
+    if macs is not None:
+        for mac in macs:
+            name = get_name_by_mac(mac)
+            activate_dhcp(name, family)
+
     sys.exit(1001)
 else:
     sys.exit(0)
-
